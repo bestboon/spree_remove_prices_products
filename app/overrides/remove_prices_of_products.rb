25125fca75286ff738378a5cd838f1a1a5a0d215
Victor Archila <varchila92@gmail.com>
2014-11-05 18:55:30 -0430

    Carga inicial de la extensión de spree que permite remover los precios de los productos del catalogo y el total de una orden.

---
+++ app/overrides/remove_prices_of_products.rb
@@ -0,0 +1,3 @@
+Deface::Override.new(:virtual_path => "spree/shared/_products",
+                 :name => "footer-left",
+                 :remove => ".price")
---
+++ lib/spree_remove_prices_products.rb
@@ -0,0 +1,2 @@
+require 'spree_core'
+require 'spree_remove_prices_products/engine'
